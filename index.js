60307ef2791b754ddc740f3736b681f32ec3abe3
Isaac <govegeku00@gmail.com>
2023-10-16 13:03:34 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here
+const Element = document.getElementById("main")
+Element.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+
+newHeader.textContent = "Isaac is the champion"
